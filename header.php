7830781060d96f05c73ee9805a2c8e95bac7f736
Paul Charpie <paulc@theaamgroup.com>
2021-09-27 02:14:15 +0000

    header layout complete

---
+++ header.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Grechen+Fuemen&display=swap');
+    </style>
+    <title>Farrah&#86;s Goldens - Tennessee Golden Retriever Breeding Program</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class='col-sm-12 head-container'>
+                <div class="col-sm-12 text-center logo">
+                    <h1>Farrah's Goldens</h1>
+                </div>
+                <nav class="navbar navbar-light navbar-expand-sm" id='main-nav' role='tablist'>
+                    <ul class="nav nav-tabs container-fluid text-center" id="main-navbar">
+                        <li class='nav-item nav-bar-item flex-fill active' role="presentation"><a class='nav-link active' id="home-tab-nav" data-bs-toggle="tab" data-bs-target="#home-tab" type="button" role="tab" aria-controls="home" aria-selected="true">Home</a></li>
+                        <li class='nav-item nav-bar-item flex-fill' role="presentation"><a class='nav-link' id="bio-tab-nav" data-bs-toggle="tab" data-bs-target="#bio-tab" type="button" role="tab" aria-controls="bio" aria-selected="false">Bio</a></li>
+                        <li class='nav-item nav-bar-item flex-fill' role="presentation"><a class='nav-link' href='litter.php'>Litters</a></li>
+                        <li class='nav-item nav-bar-item flex-fill' role="presentation"><a class='nav-link' href='about-us.php'>About Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include './header';
+<?php include 'header.php'; ?>
+
+<div class="row">
+    <div class="tab-content">
+        <div class="tab-pane active" role="tabpanel" aria-labelledby="home-tab-nav" id="home-tab">
+            <div class="col-sm-4">Sidebar</div>
+            <div class="col-sm-8">Content</div>
+        </div>
+        <div class="tab-pane" role="tabpanel" aria-labelledby="bio-tab-nav" id="bio-tab">
+            This is the bio
+        </div>
+    </div>
+
+</div>
+</div>
+
+</body>
+
+</html>
